fd7e8077fdcd279f063e7ea896f7a9d0ac0d0e5c
harsh <harshhate5@gmail.com>
2024-07-21 15:44:13 +0530

    first commit

---
+++ Python_Day1.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Learning the basic print statement in Python
+
+# In[1]:
+
+
+print("Hello World!")
+
+
+# In[2]:
+
+
+print("10")
+
+
+# In[3]:
+
+
+print("Hello! This is Harsh.")
+
+
+# In[4]:
+
+
+"Hello World"
+
+
+# In[5]:
+
+
+x = 25
+print(x)
+
+
+# In[6]:
+
+
+x = 40
+print(x+13)
+
+
+# In[7]:
+
+
+number = 7
+print(number)
+
+
+# In[8]:
+
+
+number = 24 + 16
+print(number)
+
+
+# In[9]:
+
+
+number = 10
+
+
+# In[10]:
+
+
+print(number)
+
+
+# In[11]:
+
+
+print(number+10)
+
+
+# In[12]:
+
+
+number = (number+10)
+print(number)
+
+
+# In[ ]:
+
+
+
+
