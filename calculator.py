bc9084a4b11e2a8887177b252fbeb11c37bf4832
harsh <harshhate5@gmail.com>
2024-08-06 22:20:21 +0530

    n

---
+++ calculator.py
@@ -0,0 +1,29 @@
+def add(num1, num2):
+    return num1 + num2
+
+def subtraction(num1, num2):
+    return abs(num1-num2)
+
+def multiplication(num1, num2):
+    return num1*num2
+
+def integer_division(num1, num2):
+    return num1//num2
+
+def float_division(num1, num2):
+    return num1/num2
+
+def mod(num1, num2):
+    return num1%num2
+
+def percentage(num1, num2):
+    return (num1/num2) * 100
+
+def power(num1, num2):
+    return num1**num2
+
+def factorial(num1):
+    if num1==0:
+        return 1
+    return num1 * factorial(num-1)
+ 
